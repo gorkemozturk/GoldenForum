c5cb3881c87376d3409b5d5643ba325cdd7ab993
Görkem Öztürk <gorkemozturkse@gmail.com>
2019-06-01 16:59:55 +0300

    Initial data configuration has been implemented

---
+++ GoldenForum.Service/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GoldenForum.Service.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldenForum.Service.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+
+}
---
+++ GoldenForum.Service/Models/Reply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldenForum.Service.Models
+{
+    public class Reply
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime RepliedAt { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
